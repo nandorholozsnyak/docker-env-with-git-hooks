5a8fd6219556f0fd08082fa8d7307d6408e0a6f1
Nándor Holozsnyák <nandor.holozsnyak@gmail.com>
2022-10-31 21:59:23 +0100

    Init

---
+++ src/main/java/co/rodnansol/githooks/DockerEnvWithGitHooksApplication.java
@@ -0,0 +1,13 @@
+package co.rodnansol.githooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEnvWithGitHooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerEnvWithGitHooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/rodnansol/githooks/DockerEnvWithGitHooksApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rodnansol.githooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEnvWithGitHooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
